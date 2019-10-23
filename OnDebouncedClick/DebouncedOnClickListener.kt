b3b0a424d05e55a61bae6b62c72b32cc91d56752
Vu Huong <huongvu1397@gmail.com>
2019-10-23 11:50:08 +0700

    add on debounced click

---
+++ OnDebouncedClick/DebouncedOnClickListener.kt
@@ -0,0 +1,43 @@
+package com.example.snippingtool.views
+
+import android.view.View
+import java.lang.System.currentTimeMillis
+
+private const val DEFAULT_DEBOUNCE_INTERVAL = 750L
+
+abstract class DebouncedOnClickListener(
+  private val delayBetweenClicks: Long = DEFAULT_DEBOUNCE_INTERVAL
+) : View.OnClickListener {
+  private var lastClickTimestamp = -1L
+
+  @Deprecated(
+      message = "onDebouncedClick should be overridden instead.",
+      replaceWith = ReplaceWith("onDebouncedClick(v)")
+  )
+
+  override fun onClick(v: View) {
+    val now = currentTimeMillis()
+    if (lastClickTimestamp == -1L || now >= (lastClickTimestamp + delayBetweenClicks)) {
+      onDebouncedClick(v)
+    }
+    lastClickTimestamp = now
+  }
+
+  abstract fun onDebouncedClick(v: View)
+}
+
+/**
+ * Sets a click listener that prevents quick repeated clicks.
+ *
+ */
+fun View.onDebouncedClick(
+  delayBetweenClicks: Long = DEFAULT_DEBOUNCE_INTERVAL,
+  click: (view: View) -> Unit
+) {
+  setOnClickListener(object : DebouncedOnClickListener(delayBetweenClicks) {
+    override fun onDebouncedClick(v: View) = click(v)
+  })
+}
+
+
+
